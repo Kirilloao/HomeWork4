9629d6950607bc759f3af4dc6467615153dd99f6
Kirilloao <kirilltr@list.ru>
2022-12-28 21:41:10 +0100

    Initial Commit

---
+++ HomeWork4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork4
+//
+//  Created by Kirill Taraturin on 28.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
